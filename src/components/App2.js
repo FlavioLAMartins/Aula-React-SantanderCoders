7e4c3dc18a1f178ca18c60f53f0042f1e7b80199
FlavioLAMartins <flavio_martins11@hotmail.com>
2021-07-20 18:47:57 -0300

    add App3.js

--- src/components/App2.js
+++ src/components/App2.js
@@ -6,8 +6,9 @@ class  App2 extends React.Component {
     }
   render() {
     return (
-    <div>
-
+    <div className="box">
+        <div id="title">{this.props.title}</div>
+        <div id="text">{this.props.children}</div>
     </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App2 />
+    <App2 title="Título">
+      Conteúdo
+      </App2>
+
+      <App2 title="Título2">
+      Conteúdo2
+      </App2>
   </React.StrictMode>,
   document.getElementById('root')
 );
